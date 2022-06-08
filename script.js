9ccc21af5f3fa8f40b427f8a21340322adcf3f03
nasramo <103969243+nasramo@users.noreply.github.com>
2022-06-08 19:08:55 +0100

    Shortened js file
    Backup just in case repl messes up

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//work on smoother sidebar (maybe css transitions) and greying when active - maybe z-index w position: fixed/absolute/relative???
-//work on checkbox showing/hiding different exams
-/*finish off css colour assignment for boxes, test on 'mobile' using dev tools for iphone se 1st gen/5, fix the sidebar*/
-
 $(document).ready(function(){
   $('[data-toggle="popover"]').popover({
       container: 'body'
@@ -10,18 +6,3 @@ $(document).ready(function(){
 $('.popover-dismiss').popover({
   trigger: 'focus'
 });
-
-/*
-let checkbio = document.querySelector('input[value="biology"]');
-let exbio = document.getElementsByName('bio');
-exbio.style.visibility = 'hidden';
-
-checkbio.addEventListener('change', () => {
-  if(checkbio.checked) {
-    exbio.style.visibility = 'visible';
-
-  } else {
-    exbio.style.visibility = 'hidden';
-  }
-});*/
-
